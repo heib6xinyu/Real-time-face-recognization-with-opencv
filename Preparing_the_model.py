74446f7352972426200ee6adcee63236287a6bf2
heib6xinyu <78740107+heib6xinyu@users.noreply.github.com>
2024-02-26 17:05:54 -0500

    Update Preparing_the_model.py
    uncommend some lines

--- Preparing_the_model.py
+++ Preparing_the_model.py
@@ -11,17 +11,17 @@ output_dir = "data\\cropped_faces"
 output_processed_dir = "data\\pre_processed"
 
 #read_images_from_folder(input_dir, output_dir)
-#process_images_from_folder(output_dir, output_processed_dir)
+process_images_from_folder(output_dir, output_processed_dir)
 
-#faces, labels, label_dict= prepare_training_data(output_processed_dir)
+faces, labels, label_dict= prepare_training_data(output_processed_dir)
 
-#faces = np.array(faces)
-#labels = np.array(labels)
+faces = np.array(faces)
+labels = np.array(labels)
 
-#recognizer = cv2.face.LBPHFaceRecognizer_create()
-##train the recognizer
-#recognizer.train(faces, labels)
-#recognizer.write('model.yml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+#train the recognizer
+recognizer.train(faces, labels)
+recognizer.write('model.yml')
 
-#with open("data\\label_dict.json", "w") as file:
-#    json.dump(label_dict, file)
+with open("data\\label_dict.json", "w") as file:
+    json.dump(label_dict, file)
